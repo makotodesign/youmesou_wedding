e62e8b300bc60035c8667bd5d8c5bfcd70527b73
makotodesign <makoto@makotodesign.net>
2021-07-23 08:01:01 +0900

    header footer ok

--- root/wp/wp-content/themes/base/parts_footer.php
+++ root/wp/wp-content/themes/base/parts_footer.php
@@ -16,38 +16,27 @@
 		<footer class="footer_wrap">
 			<div class="footer">
 				<address class="footer_profile">
-					<p class="footer_name">COMPANYNAME</p>
-					<p class="footer_address">〒000-0000<br>ADDRESS</p>
+					<p class="footer_name"><img src="/images/common/ci.png" alt="結水荘（ゆうみそう）" width="40px">結水荘（ゆうみそう）</p>
+					<p class="footer_address">〒655-0033<br>兵庫県神戸市垂水区旭が丘1丁目5-26 </p>
 					<div class="footer_tel_set">
-						<p class="footer_tel mark_title tel" title="TEL"><a href="tel:000-000-0000">000-000-0000</a></p>
-						<p class="footer_fax mark_title fax" title="FAX"><a href="tel:000-000-0000">000-000-0000</a></p>
+						<p class="footer_tel mark_title tel" title="TEL"><a href="tel:080-7037-4947">080-7037-4947</a></p>
 					</div>
 				</address>
 				<nav class="footer_sitenav">
 					<div class="footer_sitenav_cont">
 						<ul>
-							<li><a href="/xx/" class="icon_arrow">MENU</a></li>
-							<li><a href="/xx/" class="icon_arrow">MENU</a></li>
-							<li><a href="/xx/" class="icon_arrow">MENU</a></li>
-						</ul>
-					</div>
-					<div class="footer_sitenav_cont">
-						<ul>
-							<li><a href="/xx/" class="icon_arrow">MENU</a></li>
-							<li><a href="/xx/" class="icon_arrow">MENU</a></li>
-						</ul>
-					</div>
-					<div class="footer_sitenav_cont">
-						<h4 class="footer_sitenav_heading">メニュータイトル</h4>
-						<ul>
-							<li><a href="/xx/" class="icon_arrow">MENU</a></li>
-							<li><a href="/xx/" class="icon_arrow">MENU</a></li>
+							<li><a href="/" class="icon_arrow">HOME</a></li>
+							<li><a href="/plan/" class="icon_arrow">プラン</a></li>
+							<li><a href="/weblog/" class="icon_arrow">結水荘日記</a></li>
+							<li><a href="/member/" class="icon_arrow">メンバー紹介</a></li>
+							<li><a href="/english/" class="icon_arrow">English</a></li>
+							<li><a href="/contact/" class="icon_arrow">お問い合わせ</a></li>
 						</ul>
 					</div>
 				</nav>
 			</div>
 			<div class="copyright_wrap">
-				<p class="copyright"><small>&copy; Copyright . All Rights Reserved.</small></p>
+				<p class="copyright"><small>&copy; Copyright . Youmeso All Rights Reserved.</small></p>
 			</div>
 		</footer>
 		<div class="pagetop">
--- root/wp/wp-content/themes/base/parts_header.php
+++ root/wp/wp-content/themes/base/parts_header.php
@@ -18,56 +18,17 @@
 			<div class="header_topnav_wrap hide_sp_tb">
 				<div class="header_topnav">
 					<ul class="ecnav_list">
-						<li><a href="/ec/mypage/login" id="topnav_ec_login" class="v_ec_login <?= $ec_login_class ?>"><span>ログイン</span></a></li>
-						<li><a href="/ec/entry" id="topnav_ec_register" class="v_ec_register <?= $ec_login_class ?>"><span>会員登録</span></a></li>
-						<li><a href="/ec/logout" id="topnav_ec_logout" class="v_ec_logout <?= $ec_login_class ?>"><span>ログアウト</span></a></li>
-						<li><a href="/ec/mypage/favorite" id="topnav_ec_favorite" class="v_ec_favorite <?= $ec_login_class ?>"><span>お気に入り</span></a></li>
-						<li><a href="/ec/mypage/" id="topnav_ec_mypage" class="v_ec_mypage <?= $ec_login_class ?>"><span>MYページ</span></a></li>
+						<li><a href=""  class=""><span>FB</span></a></li>
+						<li><a href=""  class=""><span>TW</span></a></li>
+						<li><a href=""  class=""><span>English</span></a></li>
 					</ul>
 				</div>
 			</div>
 			<div class="header">
 				<a class="logo_set" href="/">
-					<p class="logo"><span><img src="/images/common/logo.svg" width="300" height="60" alt="必ず記述"></span></p>
+					<p class="logo"><span><img src="/images/common/logo_wedding.png" width="300" alt="結水荘ウェディング"></span></p>
 				</a>
-<?php
-	/* memo */
-	// base.jsで hnav_list がhnav_wrapに囲まれた上でここに移動します
-	/* pc : hnav ナビや検索、電話番号など表示 */
-	// markup はgnav内に記載
-/*
-				<nav class="hnav">
-					<ul class="hnav_list">
-						<li><a href="/xx/" class="icon_arrow"><span>MENU</span></a></li>
-						<li><a href="/xx/" class="icon_arrow"><span>MENU</span></a></li>
-					</ul>
-					<form class="hsearch" method="post" action="">
-						<div class="search_cont">
-							<p class="input_wrap"><input type="text" name="" place_polder="Search･･･"></p>
-							<button type="submit"><span>検索</span></button>
-						</div>
-					</form>
-					<div class="langnav">
-						<p class="lang_current"><span>Ja</span></p>
-						<ul class="langnav_list">
-							<li><a href="/en/"><span>En</span></a></li>
-						</ul>
-					</div>
-					<div class="hcontact">
-						<p class="header_tel"><a href="tel:000-000-0000"><span class="icon_tel">TEL</span><span>000-000-0000</span></a></p>
-					</div>
-				</nav>
-*/
-?>
 				<p class="gnav_btn menu"><span></span><span></span><span></span></p>
-<?php
-/* gnav が headerの「下」or「横」 */
-// 下の場合は下記 ganv_wrap 必要
-/*
-			</div>
-			<div class="gnav_wrap">
-*/
-?>
 				<nav class="gnav">
 					<ul class="topnav_list hide_pc">
 						<li><a href="/xx/"><span>MENU</span></a></li>
@@ -75,30 +36,10 @@
 					</ul>
 					<ul class="gnav_list">
 						<li><a href="/" data-gnav="home"><span>HOME</span></a></li>
-						<li><a href="/aaa/"><span>MENU_A</span></a></li>
-						<li class="submenu_wrap">
-							<a href="/bbb/"><span>MENU_B</span></a>
-							<div class="submenu">
-								<ul>
-									<li><a href="/bbb/bb01/"><span>MENU_B_01</span></a></li>
-									<li><a href="/bbb/bb02/"><span>MENU_B_02</span></a></li>
-								</ul>
-							</div>
-						</li>
-						<li class="submenu_wrap">
-							<a href="/ccc/"><span>MENU_C</span></a>
-							<div class="submenu">
-								<ul>
-									<li><a href="/xx/" ><span>xx</span></a></li>
-									<li><a href="/xx/sample_markup.php" ><span>sample_markup</span></a></li>
-									<li><a href="/contact/" ><span>contact</span></a></li>
-									<li><a href="/news/" ><span>news</span></a></li>
-									<li><a href="/productsec/65" ><span>productsec</span></a></li>
-								</ul>
-							</div>
-						</li>
-						<li><a href="/ddd/"><span>MENU_D</span></a></li>
-						<li><a href="/eee/" data-navcurrent="false"><span>MENU_E</span></a></li>
+						<li><a href="/plan/"><span>プラン紹介</span></a></li>
+						<li><a href="/weblog/"><span>結水荘日記</span></a></li>
+						<li><a href="/member/"><span>メンバー紹介</span></a></li>
+						<li><a href="/contact/"><span>お問い合わせ</span></a></li>
 					</ul>
 				</nav>
 			</div>
